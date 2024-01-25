b9237b67cf6ded3d7f53c29f8b9b91359c4ae316
mehrnaz <pishyar.mehrnaz@gmail.com>
2024-01-25 22:23:44 +0100

    create navbar and menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import "./App.scss";
+
 import Header from "./components/Header/Header";
-import Slider from "./components/Slider/Slider"
-import Main from './components/Main/Main'
-import Info from './components/Info/Info'
-import Testimonial from './components/Testimonial/Testimonial'
-import FAQ from './components/FAQ/FAQ'
-import Footer from './components/Footer/Footer'
+import Slider from "./components/Slider/Slider";
+import Main from "./components/Main/Main";
+import Info from "./components/Info/Info";
+import Testimonial from "./components/Testimonial/Testimonial";
+import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer/Footer";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return <div>
-  <Header/>
-  <Slider/>
-  <Main/>
-  <Info/>
-  <Testimonial/>
-  <FAQ/>
-  <Footer/>
-  </div>;
+  return (
+    <div>
+      <Navbar />
+      <Header />
+      <Slider />
+      <Main />
+      <Info />
+      <Testimonial />
+      <FAQ />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,56 @@
-import './Navbar'
+import "./Navbar.css";
+
+import { FaSearch } from "react-icons/fa";
+import {
+  RiHeartFill,
+  RiShoppingCartFill,
+} from "react-icons/ri";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header>
+      <nav>
+        <div className="tn-container container">
+          <a href="" className="logo">
+            Shopping
+          </a>
+          <div className="search-box">
+            <input
+              type="search"
+              name="search"
+              id=""
+              placeholder="Search For Products..."
+            />
+            <div className="search-icon">
+              <FaSearch />
+            </div>
+          </div>
+          <div className="tn-icons">
+            <a href="" className="fav-icon">
+              <RiHeartFill /> <span className="count">0</span>
+            </a>
+            <a href="" className="cart-icon">
+              <RiShoppingCartFill /><span className="count">0</span>
+            </a>
+          </div>
+        </div>
+        <hr />
+        <div className="bn-container container">
+          <ul className="navlist">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#all">AllProducts</a>
+            </li>
+            <li>
+              <a href="Download">Download</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
